8565be92dd94e51bdc322555b818425fad0653a2
elcordova <erick.lenin.03@gmail.com>
2021-02-07 03:20:25 -0500

    added service onnection race

--- js/carService.js
+++ js/carService.js
@@ -2,9 +2,29 @@ import {API_URL_LATEST} from "./constats.js";
 import {appendCards} from "./template.js";
 import {addCars, getCars, getLastId} from "./clientStorage.js"
 export const loadCars = async () =>{
-  await loadCarsRequest();
+  document.getElementById("connection-status").innerHTML = await fetchPromise();
   const cars = await getCars();
   appendCards(cars);
+}
+
+const fetchPromise = () => {
+  const getOnlineDataPromise = new Promise(async (resolve) =>{
+    try {
+      await loadCarsRequest();
+    } catch (err) {
+      resolve("No connection, showing online results");
+    }
+    resolve("This connection is OK, showing the bake");
+  });
+
+  const hangingsPromise = new Promise((resolve) => {
+    setTimeout(
+      resolve,
+      3000, 
+      "The connection is Hanging, showing offline results"
+    );
+  });
+  return Promise.race([hangingsPromise, getOnlineDataPromise]);
 }
 
 export const loadCarsRequest = async() => {
